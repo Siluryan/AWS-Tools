a3cd5a387576bfb77a36ba17bb63543a0d5166ba
Guilherme Dias <guilherme.dias@tecsinapse.com.br>
2023-05-27 16:28:57 -0300

    diversas alterações

--- transferHostedZones/exportHostedZone.sh
+++ transferHostedZones/exportHostedZone.sh
@@ -1,45 +1,42 @@
 #!/bin/bash
 
-# Solicita as informações de IAM da zona hospedada de origem
-read -p "Digite o ID do role da IAM da zona hospedada de origem: " source_role_id
-read -p "Digite o nome do role da IAM da zona hospedada de origem: " source_role_name
-read -p "Digite o nome da sessão da IAM da zona hospedada de origem: " source_session_name
+read -p "Digite o ID do role da IAM da zona hospedada de destino: " destination_role_id
+read -p "Digite o nome do role da IAM da zona hospedada de destino: " destination_role_name
+read -p "Digite o nome da sessão da IAM da zona hospedada de destino: " destination_session_name
 
-# Assume o role da IAM da zona hospedada de origem
-echo "Assumindo o role da IAM da zona hospedada de origem..."
-source_assume_role=$(aws sts assume-role --role-arn arn:aws:iam::$source_role_id:role/$source_role_name --role-session-name $source_session_name)
-source_access_key=$(echo $source_assume_role | jq -r '.Credentials.AccessKeyId')
-source_secret_key=$(echo $source_assume_role | jq -r '.Credentials.SecretAccessKey')
-source_session_token=$(echo $source_assume_role | jq -r '.Credentials.SessionToken')
+echo "Assumindo o role da IAM da zona hospedada de destino..."
+destination_assume_role=$(aws sts assume-role --role-arn arn:aws:iam::$destination_role_id:role/$destination_role_name --role-session-name $destination_session_name)
 
-# Configura as credenciais da zona hospedada de origem
-echo "Configurando as credenciais da zona hospedada de origem..."
-unset AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
-export AWS_ACCESS_KEY_ID=$source_access_key
-export AWS_SECRET_ACCESS_KEY=$source_secret_key
-export AWS_SESSION_TOKEN=$source_session_token
+if [ $? -eq 0 ]; then
+    destination_access_key=$(echo $destination_assume_role | jq -r '.Credentials.AccessKeyId')
+    destination_secret_key=$(echo $destination_assume_role | jq -r '.Credentials.SecretAccessKey')
+    destination_session_token=$(echo $destination_assume_role | jq -r '.Credentials.SessionToken')
 
-# Solicita as informações da zona hospedada de origem
+    echo "Configurando as credenciais da zona hospedada de destino..."
+    aws configure set aws_access_key_id "$destination_access_key"
+    aws configure set aws_secret_access_key "$destination_secret_key"
+    aws configure set aws_session_token "$destination_session_token"
+
+    echo "Você assumiu a role com sucesso!"
+else
+    echo "Falha ao assumir a role. Verifique se as credenciais e o ARN da role estão corretos."
+fi
+
 read -p "Digite o ID da zona hospedada de origem: " source_zone_id
 read -p "Digite o nome do arquivo JSON de saída: " json_file
 
-# Listar registros DNS da zona hospedada de origem
 echo "Listando registros DNS da zona hospedada de origem..."
 source_records=$(aws route53 list-resource-record-sets --hosted-zone-id $source_zone_id | jq '.ResourceRecordSets')
 
-# Filtrar registros DNS da zona hospedada de origem
 echo "Filtrando registros DNS da zona hospedada de origem..."
 source_filtered_records=$(echo $source_records | jq 'map(select(.Type != "NS" and .Type != "SOA"))')
 
-# Adicionar a ação "CREATE" para cada registro da zona hospedada de origem
 echo "Adicionando a ação CREATE para cada registro da zona hospedada de origem..."
 source_created_records=$(echo $source_filtered_records | jq 'map({Action: "CREATE", ResourceRecordSet: .})')
 
-# Converter para o formato de mudanças da zona hospedada de origem
 echo "Convertendo para o formato de mudanças da zona hospedada de origem..."
 source_changes=$(echo $source_created_records | jq '{Changes: .}')
 
-# Salvar mudanças no arquivo JSON
 echo "Salvando mudanças no arquivo JSON..."
 echo $source_changes | jq . > $json_file
 
--- transferHostedZones/importHostedZone.sh
+++ transferHostedZones/importHostedZone.sh
@@ -1,27 +1,30 @@
-# Solicita as informações de IAM da zona hospedada de destino
+#!/bin/bash
+
 read -p "Digite o ID do role da IAM da zona hospedada de destino: " destination_role_id
 read -p "Digite o nome do role da IAM da zona hospedada de destino: " destination_role_name
 read -p "Digite o nome da sessão da IAM da zona hospedada de destino: " destination_session_name
 
-# Assume o role da IAM da zona hospedada de destino
 echo "Assumindo o role da IAM da zona hospedada de destino..."
 destination_assume_role=$(aws sts assume-role --role-arn arn:aws:iam::$destination_role_id:role/$destination_role_name --role-session-name $destination_session_name)
-destination_access_key=$(echo $destination_assume_role | jq -r '.Credentials.AccessKeyId')
-destination_secret_key=$(echo $destination_assume_role | jq -r '.Credentials.SecretAccessKey')
-destination_session_token=$(echo $destination_assume_role | jq -r '.Credentials.SessionToken')
 
-# Configura as credenciais da zona hospedada de destino
-echo "Configurando as credenciais da zona hospedada de destino..."
-unset AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
-export AWS_ACCESS_KEY_ID=$destination_access_key
-export AWS_SECRET_ACCESS_KEY=$destination_secret_key
-export AWS_SESSION_TOKEN=$destination_session_token
+if [ $? -eq 0 ]; then
+    destination_access_key=$(echo $destination_assume_role | jq -r '.Credentials.AccessKeyId')
+    destination_secret_key=$(echo $destination_assume_role | jq -r '.Credentials.SecretAccessKey')
+    destination_session_token=$(echo $destination_assume_role | jq -r '.Credentials.SessionToken')
 
-# Solicita as informações da zona hospedada de destino
+    echo "Configurando as credenciais da zona hospedada de destino..."
+    aws configure set aws_access_key_id "$destination_access_key"
+    aws configure set aws_secret_access_key "$destination_secret_key"
+    aws configure set aws_session_token "$destination_session_token"
+
+    echo "Você assumiu a role com sucesso!"
+else
+    echo "Falha ao assumir a role. Verifique se as credenciais e o ARN da role estão corretos."
+fi
+
 read -p "Digite o ID da zona hospedada de destino: " hosted_zone_id
 read -p "Digite o nome do arquivo JSON com as mudanças: " json_file
 
-# Realiza a mudança de conjunto de registros na zona hospedada
 echo "Realizando mudança de conjunto de registros na zona hospedada..."
 aws route53 change-resource-record-sets --hosted-zone-id $hosted_zone_id --change-batch file://$json_file
 
