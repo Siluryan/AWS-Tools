ca5460a08e2ccdd77215e94e70b668c9015290bf
guilherme.dias <guilhermerrdias@gmail.com>
2024-03-14 16:25:15 -0300

    em desenvolvimento

--- lambdas/serverless/backupAndRemoveEC2Files/backupAndRemove.py
+++ lambdas/serverless/backupAndRemoveEC2Files/backupAndRemove.py
@@ -1,43 +1,49 @@
-import boto3
 import os
+import boto3
 import paramiko
+import tempfile
 from zipfile import ZipFile
 from datetime import datetime
-import tempfile
 
 ec2 = boto3.client('ec2')
 s3 = boto3.client('s3')
 ssm = boto3.client('ssm')
 
 def lambda_handler(event, context):
-    INSTANCE_ID = os.getenv('INSTANCE_ID')
-    BUCKET_NAME = os.getenv('BUCKET_NAME')
-    PATH = os.getenv('PATH')
-    YEAR_LIMIT = os.getenv('YEAR_LIMIT')
-    SSH_PRIVATE_KEY = os.getenv('SSH_PRIVATE_KEY_PARAMETER_STORE_NAME')
+    try:
+        INSTANCE_ID = os.getenv('INSTANCE_ID')
+        BUCKET_NAME = os.getenv('BUCKET_NAME')
+        PATH = os.getenv('PATH')
+        YEAR_LIMIT = int(os.getenv('YEAR_LIMIT'))
+        SSH_PRIVATE_KEY = os.getenv('SSH_PRIVATE_KEY_PARAMETER_STORE_NAME')
 
-    response = ec2.describe_instances(InstanceIds=[INSTANCE_ID])
-    internal_ip = response['Reservations'][0]['Instances'][0]['PrivateIpAddress']
+        response = ec2.describe_instances(InstanceIds=[INSTANCE_ID])
+        internal_ip = response['Reservations'][0]['Instances'][0]['PrivateIpAddress']
 
-    key_param = ssm.get_parameter(Name=SSH_PRIVATE_KEY, WithDecryption=True)
-    private_key = key_param['Parameter']['Value']
+        key_param = ssm.get_parameter(Name=SSH_PRIVATE_KEY, WithDecryption=True)
+        private_key = key_param['Parameter']['Value']
 
-    files = get_files(internal_ip, PATH, private_key)
+        files = get_files(internal_ip, PATH, private_key)
 
-    filtered_files = [file for file in files if datetime.strptime(file['lastModifiedDate'], '%Y-%m-%d %H:%M:%S').year < YEAR_LIMIT]
+        filtered_files = [file for file in files if datetime.strptime(file['lastModifiedDate'], '%Y-%m-%d %H:%M:%S').year < YEAR_LIMIT]
 
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_zip_file:
-        with ZipFile(tmp_zip_file, 'w') as zipf:
-            for file in filtered_files:
-                download_file(internal_ip, PATH, file['fileName'], zipf, private_key)
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_zip_file:
+            with ZipFile(tmp_zip_file, 'w') as zipf:
+                for file in filtered_files:
+                    download_file(internal_ip, PATH, file['fileName'], zipf, private_key)
 
-        tmp_zip_file.seek(0)
-        s3.upload_fileobj(tmp_zip_file, BUCKET_NAME, 'files.zip')
+            tmp_zip_file.seek(0)
+            s3.upload_fileobj(tmp_zip_file, BUCKET_NAME, 'files.zip')
 
-    return {
-        'statusCode': 200,
-        'body': 'Files transferred and compressed successfully.'
-    }
+        return {
+            'statusCode': 200,
+            'body': 'Files transferred and compressed successfully.'
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error: {str(e)}'
+        }
 
 def get_files(ip_address, PATH, private_key):
     ssh = paramiko.SSHClient()
